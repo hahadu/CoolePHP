6c77ea67163085d0c27faa9176b5d9df339ae940
hahadu <mhyzhu@163.com>
2020-09-08 20:32:04 +0800

    add test

--- test/index.php
+++ test/index.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors',1);    //php启动错误信息
 error_reporting(-1);                    //打印出所有的 错误信息  
 ini_set('error_log', dirname(__FILE__) . '/error_log.txt'); //将出错信息输出到一个文本文件
 
-require 'CoolePHP/CoolePHP.php';
-require 'CoolePHP/vendor/autoload.php';
+require '../CoolePHP.php';
+
 
 
 
