c4f337f4354e71fc3886e5e6eb2fda38a4151e87
hahadu <mhyzhu@163.com>
2020-09-24 11:25:49 +0800

    add function files

---
+++ src/common/function.php
@@ -0,0 +1,33 @@
+<?php
+
+/****
+ * @param $var
+ * @param bool $echo
+ * @param null $label
+ * @param bool $strict
+ * @return false|string|null
+ */
+function dump($var, $echo=true, $label=null, $strict=true) {
+    $label = ($label === null) ? '' : rtrim($label) . ' ';
+    if (!$strict) {
+        if (ini_get('html_errors')) {
+            $output = print_r($var, true);
+            $output = '<pre style="color:#fafafa;">' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        } else {
+            $output = $label . print_r($var, true);
+        }
+    } else {
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+        if (!extension_loaded('xdebug')) {
+            $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+            $output = '<pre style="background-color:#fafafa;">' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        }
+    }
+    if ($echo) {
+        echo($output);
+        return null;
+    }else
+        return $output;
+}
