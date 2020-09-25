3801d5093433c2f76fe079d639c61ee8ac0b5020
hahadu <mhyzhu@163.com>
2020-09-26 00:45:49 +0800

    repair bug

--- src/common/function.php
+++ src/common/function.php
@@ -5,34 +5,36 @@
  * @param  string $str 文件名
  * @return string      文件格式
  */
-function file_format($str){
-    // 取文件后缀名
-    $str=strtolower(pathinfo($str, PATHINFO_EXTENSION));
-    // 图片格式
-    $image=array('webp','jpg','png','ico','bmp','gif','tif','pcx','tga','bmp','pxc','tiff','jpeg','exif','fpx','svg','psd','cdr','pcd','dxf','ufo','eps','ai','hdri');
-    // 视频格式
-    $video=array('mp4','avi','3gp','rmvb','gif','wmv','mkv','mpg','vob','mov','flv','swf','mp3','ape','wma','aac','mmf','amr','m4a','m4r','ogg','wav','wavpack');
-    // 压缩格式
-    $zip=array('rar','zip','tar','cab','uue','jar','iso','z','7-zip','ace','lzh','arj','gzip','bz2','tz');
-    // 文档格式
-    $text=array('exe','doc','ppt','xls','wps','txt','lrc','wfs','torrent','html','htm','java','js','css','less','php','pdf','pps','host','box','docx','word','perfect','dot','dsf','efe','ini','json','lnk','log','msi','ost','pcs','tmp','xlsb');
-    // 匹配不同的结果
-    switch ($str) {
-        case in_array($str, $image):
-            return 'image';
-            break;
-        case in_array($str, $video):
-            return 'video';
-            break;
-        case in_array($str, $zip):
-            return 'zip';
-            break;
-        case in_array($str, $text):
-            return 'text';
-            break;
-        default:
-            return 'image';
-            break;
+if(function_exists('file_format')){
+    function file_format($str){
+        // 取文件后缀名
+        $str=strtolower(pathinfo($str, PATHINFO_EXTENSION));
+        // 图片格式
+        $image=array('webp','jpg','png','ico','bmp','gif','tif','pcx','tga','bmp','pxc','tiff','jpeg','exif','fpx','svg','psd','cdr','pcd','dxf','ufo','eps','ai','hdri');
+        // 视频格式
+        $video=array('mp4','avi','3gp','rmvb','gif','wmv','mkv','mpg','vob','mov','flv','swf','mp3','ape','wma','aac','mmf','amr','m4a','m4r','ogg','wav','wavpack');
+        // 压缩格式
+        $zip=array('rar','zip','tar','cab','uue','jar','iso','z','7-zip','ace','lzh','arj','gzip','bz2','tz');
+        // 文档格式
+        $text=array('exe','doc','ppt','xls','wps','txt','lrc','wfs','torrent','html','htm','java','js','css','less','php','pdf','pps','host','box','docx','word','perfect','dot','dsf','efe','ini','json','lnk','log','msi','ost','pcs','tmp','xlsb');
+        // 匹配不同的结果
+        switch ($str) {
+            case in_array($str, $image):
+                return 'image';
+                break;
+            case in_array($str, $video):
+                return 'video';
+                break;
+            case in_array($str, $zip):
+                return 'zip';
+                break;
+            case in_array($str, $text):
+                return 'text';
+                break;
+            default:
+                return 'image';
+                break;
+        }
     }
 }
 
@@ -44,27 +46,29 @@ function file_format($str){
  * @param bool $strict
  * @return false|string|null
  */
-function dump($var, $echo=true, $label=null, $strict=true) {
-    $label = ($label === null) ? '' : rtrim($label) . ' ';
-    if (!$strict) {
-        if (ini_get('html_errors')) {
-            $output = print_r($var, true);
-            $output = '<pre style="color:#fafafa;">' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+if(!function_exists('dump')){
+    function dump($var, $echo=true, $label=null, $strict=true) {
+        $label = ($label === null) ? '' : rtrim($label) . ' ';
+        if (!$strict) {
+            if (ini_get('html_errors')) {
+                $output = print_r($var, true);
+                $output = '<pre style="color:#fafafa;">' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+            } else {
+                $output = $label . print_r($var, true);
+            }
         } else {
-            $output = $label . print_r($var, true);
+            ob_start();
+            var_dump($var);
+            $output = ob_get_clean();
+            if (!extension_loaded('xdebug')) {
+                $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+                $output = '<pre style="background-color:#fafafa;">' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+            }
         }
-    } else {
-        ob_start();
-        var_dump($var);
-        $output = ob_get_clean();
-        if (!extension_loaded('xdebug')) {
-            $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
-            $output = '<pre style="background-color:#fafafa;">' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
-        }
+        if ($echo) {
+            echo($output);
+            return null;
+        }else
+            return $output;
     }
-    if ($echo) {
-        echo($output);
-        return null;
-    }else
-        return $output;
 }
